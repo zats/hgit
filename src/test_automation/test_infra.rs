213f05a2550359ac0a28f09e90fe8a1b3d5259db
Sash Zats <sash@zats.io>
2021-07-04 11:14:52 -0400

    Use built target/debug/hgit for tests

--- src/test_automation/test_infra.rs
+++ src/test_automation/test_infra.rs
@@ -19,7 +19,10 @@ pub fn hgit<'a, 'b>(command: &'a str, path: &'a Path) -> String {
     return String::from_utf8(stdout).unwrap_or("".to_string());
 }
 
-pub fn run_test_with_repo<T>(test: T) -> () where T: FnOnce(&Repository, &Path) -> () + panic::UnwindSafe {
+pub fn run_test_with_repo<T>(test: T) -> ()
+where
+    T: FnOnce(&Repository, &Path) -> () + panic::UnwindSafe,
+{
     let result = panic::catch_unwind(|| {
         let path = create_temporary_folder();
         let repo = create_git_repo(path.as_path());
@@ -29,5 +32,8 @@ pub fn run_test_with_repo<T>(test: T) -> () where T: FnOnce(&Repository, &Path) 
 }
 
 fn hgit_path() -> PathBuf {
-    PathBuf::from(file!()).join("../../../hgit").canonicalize().unwrap()
+    PathBuf::from(file!())
+        .join("../../../target/debug/hgit")
+        .canonicalize()
+        .unwrap()
 }
